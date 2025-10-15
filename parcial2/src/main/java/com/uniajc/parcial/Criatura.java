371dabb3719baf5ac0a0a144466598c516be1c00
Daniel Campo Chaguendo <Dchcampo@gmail.com>
2025-10-14 20:07:12 -0500

    Creacion de clase criatura

---
+++ parcial2/src/main/java/com/uniajc/parcial/Criatura.java
@@ -0,0 +1,23 @@
+package com.uniajc.parcial;
+
+    public abstract class Criatura {
+    protected String nombre;
+    protected int salud;
+    protected int fuerza;
+
+    public Criatura(String nombre, int salud, int fuerza) {
+        this.nombre = nombre;
+        this.salud = salud;
+        this.fuerza = fuerza;
+    }
+
+    public abstract void atacar(Criatura enemigo);
+    public abstract void defender(int daño);
+
+    public boolean estaViva() {
+        return salud > 0;
+    }
+
+    public String getNombre() { return nombre; }
+    public int getSalud() { return salud; }
+}
